6ffd6c5e5148944836e4ecc50a07880f61a8842d
Dean Flint <dean.p.flint@gmail.com>
2019-09-17 20:07:48 +0100

    Partials to the templates and added routing

--- app.js
+++ app.js
@@ -14,4 +14,33 @@ app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
+app.get('/play', (req, res) => {
+    res.render('play.ejs')
+})
+
+app.get('/predictions', (req, res) => {
+    res.render('predictions.ejs')
+})
+
+app.get('/results', (req, res) => {
+    res.render('results.ejs')
+})
+
+app.get('/profiles', (req, res) => {
+    res.render('profiles.ejs')
+})
+
+app.get('/leaderboard', (req, res) => {
+    res.render('leaderboard.ejs')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.get('/account', (req, res) => {
+    res.render('account.ejs')
+})
+
+
 app.listen(port, () => console.log(`App running on port ${port}`))
