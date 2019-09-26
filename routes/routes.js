488b91119482e275d430416926491298197ee636
Dean Flint <dean.p.flint@gmail.com>
2019-09-26 15:31:26 +0100

    sorting of ducks added

--- routes/routes.js
+++ routes/routes.js
@@ -65,6 +65,7 @@ module.exports = app => {
       .set("testQuackyRaces")
       .collection("duck")
       .find({})
+      .sort( { duckID: 1 } )
       .toArray(function(err, docs) {
         if (err) {
           console.error(err);
