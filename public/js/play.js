45b4403161c80e5401052a50f0c68b5068115df4
Dean Flint <dean.p.flint@gmail.com>
2019-10-07 10:09:02 +0100

    Play changes pushed before new branch

---
+++ public/js/play.js
@@ -0,0 +1,19 @@
+(function() {
+    // let burgerMenu = document.querySelector(".burgerMenu");
+    // let mainNav = document.querySelector(".mainNav");
+    // let mobileLoginIcon = document.querySelector(".mobileLoginIcon");
+    // let loginArea = document.querySelector(".loginArea");
+  
+    // burgerMenu.addEventListener("click", function() {
+    //   if (this.classList.contains("animateBurger")) {
+    //     this.setAttribute("class", "burgerMenu");
+    //     mainNav.setAttribute("class", "mainNav");
+    //   } else {
+    //     // resetLogin();
+    //     this.setAttribute("class", "burgerMenu animateBurger");
+    //     mainNav.setAttribute("class", "mainNav slideMenu");
+    //   }
+    // });
+ 
+  
+  })();
