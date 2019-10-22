57a43c9500fa1c15e9a94d216daf836b2e3ed79c
Ally Fincham <alexandra.fincham@skybettingandgaming.com>
2019-10-22 16:44:35 +0100

    current page highlighted

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
 console.log("TEST")
+
+
+// nav bar maintains colour maintain
+
+
+var navs = document.querySelectorAll("a.nav")
+
+navs.forEach(nav => {
+    if (nav.href == document.location.href) {
+        nav.className += " active"
+    }
+})
